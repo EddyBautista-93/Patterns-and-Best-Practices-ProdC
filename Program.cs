e134b7334eb7e9cc89befa92b3754c3dd4b78f61
EddyBautista <bautista.j.eddy@gmail.com>
2021-06-08 20:21:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Patterns_and_Best_Practices_ProdC
+{
+    class Program
+    {
+        public static void PrintStringInfo(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                Console.WriteLine($"'{s}' is null or empty");
+            }
+
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                Console.WriteLine($"'{s}' is null or whitespace");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            PrintStringInfo(null);
+            PrintStringInfo("");
+            PrintStringInfo("   ");
+            PrintStringInfo("Andrea");
+        }
+    }
+}
