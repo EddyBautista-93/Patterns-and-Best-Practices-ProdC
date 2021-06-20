0b405edf00cf81bf3ba014716ab0345179bf6350
EddyBautista <bautista.j.eddy@gmail.com>
2021-06-20 09:14:54 -0500

    http client

--- Program.cs
+++ Program.cs
@@ -1,32 +1,25 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
 
 namespace Patterns_and_Best_Practices_ProdC
 {
     class Program
     {
-        public static string Who(bool hasConsent, string name, int? age)
+        public static async Task Main()
         {
-            if (!hasConsent)
+            using(var httpClient = new HttpClient() {BaseAddress = new Uri("https://api.github.com")})
             {
-                return "No consent was given";
-            }
+                
+               var response = await httpClient.GetAsync("ors/microsoft/repos");
+               var content = await response.Content.ReadAsStringAsync();
+               Console.WriteLine(content);
 
-            if (string.IsNullOrEmpty(name))
-            {
-                return age.HasValue ? $"A {age} years old person" : "Unknown";
             }
-
-            return age.HasValue ? $"{name} ({age} years old)" : name;
         }
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine(Who(true, "Andrea", 33));
-            Console.WriteLine(Who(true, "Andrea", null));
-            Console.WriteLine(Who(true, null, 33));
-            Console.WriteLine(Who(true, null, null));
-            Console.WriteLine(Who(false, null, null));
-        }
+        
     }
-    }
+    
 }
